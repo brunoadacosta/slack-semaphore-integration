abf592514b131b903bd285efefaa426bfdc25f05
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2014-02-11 10:11:52 -0200

    fix deploy message

--- app.rb
+++ app.rb
@@ -6,13 +6,18 @@ require 'httparty'
 class SlackSemaphoreIntegration < Sinatra::Application
   post "/" do
     payload = JSON.parse(request.body.read)
-    p payload
     token = params[:token]
     channel = params[:channel]
     subdomain = params[:subdomain]
 
+    if payload["branch_name"].nil?
+      message = "[Deploy to #{payload["server_name"]}:"
+    else
+      message = "[#{payload["project_name"]} / #{payload["branch_name"]}]"
+    end
+
     commit = payload["commit"]
-    text = "[#{payload["project_name"]} / #{payload["branch_name"]}] #{payload["result"].capitalize}: #{commit["message"]} - #{commit["author_name"]}"
+    text = "#{message} #{payload["result"].capitalize}: #{commit["message"]} - #{commit["author_name"]}"
     emoji = payload["result"] == "failed" ? ":x:" : ":white_check_mark:"
 
     body = <<-BODY
